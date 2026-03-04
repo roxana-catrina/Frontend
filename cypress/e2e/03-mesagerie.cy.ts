e0f7a10cdc53cec4a0dff7aaad6eab07ad5cf9ef
roxana-catrina <roxanacatrina22@gmail.com>
2026-03-04 10:54:42 +0200

    testare mesagerie

--- cypress/e2e/03-mesagerie.cy.ts
+++ cypress/e2e/03-mesagerie.cy.ts
@@ -3,8 +3,8 @@ describe('Mesagerie Tests', () => {
     // Login înainte de fiecare test
     cy.fixture('testData').then((data) => {
       cy.visit('/authenticate');
-      cy.get('input[name="email"], input[type="email"]').type(data.users.admin.email);
-      cy.get('input[name="password"], input[type="password"]').type(data.users.admin.password);
+      cy.get('input[type="email"]').type(data.users.admin.email);
+      cy.get('input[type="password"]').type(data.users.admin.password);
       cy.get('button[type="submit"]').click();
       cy.wait(1000);
     });
@@ -14,68 +14,87 @@ describe('Mesagerie Tests', () => {
   it('16. Should load mesagerie page', () => {
     cy.visit('/mesagerie');
     cy.url().should('include', '/mesagerie');
-    cy.get('h1, h2, .mesagerie-title').should('be.visible');
+    cy.get('h2').should('contain', 'Mesagerie');
   });
 
-  // Test 17: Verifică afișarea listei de mesaje
-  it('17. Should display messages list', () => {
+  // Test 17: Verifică afișarea listei de utilizatori
+  it('17. Should display users list', () => {
     cy.visit('/mesagerie');
-    cy.get('.message-list, .messages, mat-list').should('exist');
+    cy.get('.users-list').should('be.visible');
+    cy.get('.user-card').should('exist');
   });
 
-  // Test 18: Verifică crearea unui mesaj nou
-  it('18. Should create a new message', () => {
+  // Test 18: Verifică selectarea unui utilizator și deschiderea chat-ului
+  it('18. Should open chat window when selecting a user', () => {
+    cy.visit('/mesagerie');
+    cy.get('.user-card').first().click();
+    cy.get('.chat-window').should('be.visible');
+    cy.get('.chat-messages').should('be.visible');
+  });
+
+  // Test 19: Verifică trimiterea unui mesaj
+  it('19. Should send a message to selected user', () => {
     cy.visit('/mesagerie');
     cy.fixture('testData').then((data) => {
-      cy.contains('button', /new message|nou|trimite/i).click();
-      cy.get('textarea, input[name="message"], [formControlName="continut"]')
-        .type(data.mesaj.continut);
-      cy.contains('button', /send|trimite/i).click();
-      cy.contains(data.mesaj.continut, { timeout: 10000 }).should('be.visible');
+      cy.get('.user-card').first().click();
+      cy.get('.chat-input').type(data.mesaj.continut);
+      cy.get('.btn-send').click();
+      cy.get('.message').should('contain', data.mesaj.continut);
     });
   });
 
-  // Test 19: Verifică validarea câmpurilor mesaj
-  it('19. Should validate message fields', () => {
+  // Test 20: Verifică că butonul de trimitere este dezactivat pentru mesaj gol
+  it('20. Should disable send button for empty message', () => {
     cy.visit('/mesagerie');
-    cy.contains('button', /new message|nou|trimite/i).click();
-    cy.contains('button', /send|trimite/i).click();
-    cy.get('.error-message, mat-error').should('be.visible');
+    cy.get('.user-card').first().click();
+    cy.get('.btn-send').should('be.disabled');
+    cy.get('.chat-input').type('Test');
+    cy.get('.btn-send').should('not.be.disabled');
   });
 
-  // Test 20: Verifică căutarea în mesaje
-  it('20. Should search through messages', () => {
+  // Test 21: Verifică funcționalitatea de căutare utilizatori
+  it('21. Should search through users', () => {
     cy.visit('/mesagerie');
-    cy.get('input[type="search"], input[placeholder*="search"], input[placeholder*="cauta"]')
-      .type('test');
-    cy.get('.message-item, mat-list-item').should('have.length.greaterThan', 0);
+    cy.get('.search-input').type('roxana');
+    cy.get('.user-card').should('exist');
   });
 
-  // Test 21: Verifică deschiderea unui mesaj
-  it('21. Should open and read a message', () => {
+  // Test 22: Verifică închiderea ferestrei de chat
+  it('22. Should close chat window', () => {
     cy.visit('/mesagerie');
-    cy.get('.message-item, mat-list-item').first().click();
-    cy.get('.message-content, .message-body').should('be.visible');
+    cy.get('.user-card').first().click();
+    cy.get('.chat-window').should('be.visible');
+    cy.get('.btn-close-chat').click();
+    cy.get('.chat-window').should('not.exist');
   });
 
-  // Test 22: Verifică ștergerea unui mesaj
-  it('22. Should delete a message', () => {
+  // Test 23: Verifică butonul de back
+  it('23. Should navigate back using back button', () => {
     cy.visit('/mesagerie');
-    cy.get('.message-item, mat-list-item').first().click();
-    cy.contains('button', /delete|sterge/i).click();
-    cy.contains('button', /confirm|da/i).click();
-    cy.contains('deleted|sters', { matchCase: false }).should('be.visible');
+    cy.get('.btn-back').click();
+    cy.url().should('not.include', '/mesagerie');
   });
 
-  // Test 23: Verifică actualizarea în timp real (WebSocket)
-  it('23. Should receive real-time message updates', () => {
+  // Test 24: Verifică highlight-ul utilizatorilor cu mesaje necitite
+  it('24. Should highlight users with unread messages', () => {
     cy.visit('/mesagerie');
-    // Simulăm primirea unui mesaj nou
-    cy.window().then((win) => {
-      // Verificăm că există funcționalitatea WebSocket
-      expect(win).to.have.property('WebSocket');
-    });
-    cy.wait(2000);
-    cy.get('.message-list, .messages').should('exist');
+    cy.get('.user-card').should('exist');
+    // Verifică că există cel puțin un utilizator cu mesaje necitite sau că lista se încarcă corect
+    cy.get('.user-card.has-unread, .user-card').should('have.length.greaterThan', 0);
+  });
+
+  // Test 25: Verifică clear search functionality
+  it('25. Should clear search term', () => {
+    cy.visit('/mesagerie');
+    cy.get('.search-input').type('test');
+    cy.get('.clear-btn').should('be.visible').click();
+    cy.get('.search-input').should('have.value', '');
+  });
+
+  // Test 26: Verifică afișarea butonului de partajare imagine
+  it('26. Should display image sharing button in chat', () => {
+    cy.visit('/mesagerie');
+    cy.get('.user-card').first().click();
+    cy.get('.btn-attach-image').should('be.visible');
   });
 });
